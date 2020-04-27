8ce362843d58ca7273c41efc48576764b9d9a0f3
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-04-27 13:01:34 +0300

    Pagination
    - Created pagination service for the authors cards
    - Created js file where I added all the logic
    - Imported the service
    - Created logic for filtering the authors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FiltersComponent } from './components/filters/filters.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'country/:id', component: FiltersComponent},
+  {path: '', redirectTo: '/country/all', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/pagination.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaginationService {
+  private pager: any = {};
+  private pagedItems: any[];
+  private data: any[];
+
+  constructor() { }
+
+  getPager() {
+    return this.pager;
+  }
+
+  setData(obj) {
+    this.data = obj;
+    this.goToFirstPage();
+  }
+
+  getPagedItems() {
+    return this.pagedItems;
+  }
+
+  setPager(totalItems: number, currentPage: number = 1, pageSize: number = 9) {
+    let totalPages = Math.ceil(totalItems / pageSize);
+    let startPage: number, endPage: number;
+
+    if (currentPage < 1) {
+      currentPage = 1;
+    } else if (currentPage > totalPages) {
+      currentPage = totalPages;
+    }
+
+    if (totalPages <= 10) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
+
+    let startIndex = (currentPage - 1) * pageSize;
+    let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+
+    return {
+      totalItems: totalItems,
+      currentPage: currentPage,
+      pageSize: pageSize,
+      totalPages: totalPages,
+      startPage: startPage,
+      endPage: endPage,
+      startIndex: startIndex,
+      endIndex: endIndex,
+      pages: pages
+    };
+  }
+
+  goToPage(number) {
+    this.pager = this.setPager(this.data.length, number);
+    this.pagedItems = this.data.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
+
+  goToPreviousPage() {
+    this.goToPage(this.pager.currentPage - 1);
+  }
+
+  goToNextPage() {
+    this.goToPage(this.pager.currentPage + 1);
+  }
+
+  goToFirstPage() {
+    this.goToPage(1);
+  }
+
+  goToLastPage() {
+    this.goToPage(this.pager.totalPages);
+  }
+}
